5445a2db78fcc5b3b9e4015960b43fc3661515be
Riya Sharma <124584465+RiyaSharma3@users.noreply.github.com>
2023-02-28 11:45:19 +0530

    Add files via upload

---
+++ evenORodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int n;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    if (n%2==0)
+        printf("The number is even");
+    else
+        printf("The number is odd");
+    return 0;
+}
